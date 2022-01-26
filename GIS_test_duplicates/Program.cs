3dad460b31b7a7ce2d158efa1604a406c7e1f124
Askold <Askold@LAPTOP-KUROIKR1>
2022-01-26 15:36:52 +0300

    Добавьте файлы проекта.

---
+++ GIS_test_duplicates/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GIS_test_duplicates
+{
+    class Result
+    {
+        public static void Duplicates(List<int> arr)
+        {
+            arr.Sort();
+
+            Dictionary<int, int> res = new Dictionary<int, int>();
+
+            int n = 0;
+
+            while (n < arr.Count - 1)
+            {
+
+                if (arr[n] == arr[n + 1])
+                {
+                    res.Add(arr[n], 1);
+
+                    if (n + 1 != arr.Count - 1)
+                    {
+                        while (arr[n] == arr[n + 1])
+                        {
+                            res[arr[n]]++;
+                            n++;
+                        }
+                    }
+                }
+
+                n++;
+            }
+
+            Console.WriteLine();
+            foreach (KeyValuePair<int, int> pair in res)
+                Console.WriteLine("Значение: " + pair.Key + " Кол-во: " + pair.Value);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> array = Console.ReadLine().Split(',', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            List<int> list = new List<int>();
+
+            foreach (string s in array)
+                list.Add(Convert.ToInt32(s));
+
+            Result.Duplicates(list);
+        }
+    }
+}
