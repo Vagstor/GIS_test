11593ed4fb9e1a1906221e96b1c28acfd0cd043d
Олег Костенко <askoldvagt@gmail.com>
2022-01-28 16:32:45 +0300

    task2 & task3 updates

---
+++ GIS_test_duplicates/task3_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+printf "Current date is: ";
+date;
+printf "Current processes and logged in users: ";
+w;
+printf "Operating system: ";
+uname;
+printf "Time passed since creation: ";
+uptime p;
